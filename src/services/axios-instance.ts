cd0db6b9fb2c1ff961c01b898177c71774476f10
Raymond Zeng <shilei.zeng98@outlook.com>
2024-08-10 20:57:22 -0400

    Added axions-instance

---
+++ src/services/axios-instance.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const axiosInstance = (contentType = 'application/json') => {
+  return axios.create({
+    baseURL: `${process.env.REACT_APP_API_URL || 'http://localhost:8080/api/'}`,
+    headers: {
+      'Content-Type': contentType,
+    },
+  });
+};
+
+export default axiosInstance;
