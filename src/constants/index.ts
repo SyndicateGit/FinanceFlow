4f4ed83c6a3d3b6acb138bdc82784c63f10addad
Raymond Zeng <shilei.zeng98@outlook.com>
2024-08-10 20:20:34 -0400

    Installed lucide and added sidebar icon and link constants

---
+++ src/constants/index.ts
@@ -0,0 +1,32 @@
+export const sidebarLinks = [
+  {
+    imageUrl: "../public/icons/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imageUrl: "../public/icons/accounts.svg",
+    route: "/accounts",
+    label: "Accounts",
+  },
+  {
+    imageUrl: "../public/icons/transaction-history.svg",
+    route: "/transaction-history",
+    label: "Transaction History",
+  },
+  {
+    imageUrl: "../public/icons/statistics.svg",
+    route: "/statistics",
+    label: "Statistics",
+  },
+  {
+    imageUrl: "../public/icons/create-transaction.svg",
+    route: "/create-transaction",
+    label: "Create Transaction"
+  },
+  {
+    imageUrl: "../public/icons/transfer-funds.svg",
+    route: "/transfer-funds",
+    label: "Transfer Funds",
+  },
+];
