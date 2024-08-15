81c553304ecf15a393c5364b3d547ffe3b13a8d7
Raymond Zeng <shilei.zeng98@outlook.com>
2024-08-15 11:35:42 -0400

    Added axios intercept for 401 HTTP redirect to sign in.

--- src/services/axios-instance.ts
+++ src/services/axios-instance.ts
@@ -2,13 +2,27 @@ import axios from 'axios';
 
 const axiosInstance = (contentType = 'application/json') => {
   
-  return axios.create({
+  const axiosClient = axios.create({
     baseURL: `${process.env.REACT_APP_API_URL || 'http://localhost:8080/api/'}`,
     headers: {
       'Content-Type': contentType,
       Authorization: `Bearer ${localStorage.getItem('finance_flow_auth_token')}`,
     },
   });
+
+  axiosClient.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      if (error.response.status === 401) {
+        window.location.href = '/sign-in';
+      }
+      return Promise.reject(error);
+    }
+  );
+
+  return axiosClient;
 };
 
 export default axiosInstance;
