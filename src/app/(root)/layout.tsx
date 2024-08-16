4e2cf82c898ec87bb1713556373732bd6f28f5ac
Raymond Zeng <shilei.zeng98@outlook.com>
2024-08-16 17:04:38 -0400

    Data fetch user in layout

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,30 +1,34 @@
+'use client'
 import Sidebar from "@/components/shared/Sidebar";
 import Image from "next/image";
 import Logo from "@/public/icons/LogoIcon.png"
 import MobileNav from "@/components/shared/MobileNav.";
+import { getUser } from "@/services/auth.services";
+import { defaultUser, User } from "@/Models/UserModel";
+import { useEffect, useState } from "react";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const loggedIn:User = {
-    id: "1",
-    firstName: "Raymond",
-    lastName: "Smith",
-    email: "email@email.com",
-    phone: "123-456-7890",
-    role: "USER",
-    accountIds: ["1", "2", "3"]
-  };
+  const [user, setUser] = useState<User>(defaultUser);
+  
+  useEffect(() => {
+    const fetchUser = async () => {
+      const user = await getUser();
+      setUser(user);
+    }
+    fetchUser();
+  }, []);
 
   return (
     <main className="flex h-screen w-full">
-      <Sidebar user={loggedIn}/>
+      <Sidebar user={user }/>
       <div className="flex size-full flex-col">
         <div className="flex h-16 items-center justify-between p-5 shadow-sm md:hidden">
           <Image src={Logo} width={30} height={30} alt="logo"/>
-          <MobileNav user={loggedIn} />
+          <MobileNav user={user} />
         </div>
       {children}
       </div>
